32c48b90d8ad537537cea265004019eedcd76ff5
mahendar <mahendarn915@gmail.com>
2025-09-22 12:38:30 +0530

    functions

---
+++ function.js
@@ -0,0 +1,22 @@
+// function add(){
+//     var a=4
+//     var b=9
+//     document.write(a+b);
+//     return a+b;
+// }
+
+//ES7
+// const add = ()=>{
+//     var a=4
+//     var b=6
+//     console.log(a+b)
+// }
+// add()
+
+let name = "john";
+function demo (){
+    let name = 'siva'
+    document.write(name)
+}
+document.write(name)
+demo()
