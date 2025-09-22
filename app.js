e1b51264f20807b472933ad1762f1e68763327f4
mahendar <mahendarn915@gmail.com>
2025-09-22 14:13:58 +0530

    dom

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var a=14
-var b=9
-let c=8
-const d =10
-console.log(`value c: ${c}`)
-console.log(`value d ${d}`)
+const element = document.getElementById("hello")
+
+const demo =()=>{
+    element.textContent="Mahendar"
+    element.style.backgroundColor="red"
+}
