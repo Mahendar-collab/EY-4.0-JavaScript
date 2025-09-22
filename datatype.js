b93d4802f8416543657ee3a2ec23ea1841c0b886
mahendar <mahendarn915@gmail.com>
2025-09-22 11:03:55 +0530

    changes

---
+++ datatype.js
@@ -0,0 +1,17 @@
+c=34
+s="mahendar"
+data=null
+a=true
+d = undefined
+array = (34,"jahn",true,null)
+object = {
+    name:"mahendar",
+    rollNO:1
+}
+console.log(`value c: ${typeof(c)}`)
+console.log(typeof(s))
+console.log(typeof(data))
+console.log(typeof(d))
+console.log(typeof(a))
+console.log(typeof(array))
+console.log(typeof(object))
