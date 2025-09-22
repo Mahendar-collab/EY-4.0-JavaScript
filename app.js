06e275129bcbf7ea70277ee26db937884a549b55
mahendar <mahendarn915@gmail.com>
2025-09-22 15:14:50 +0530

    Aricle

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const element = document.getElementById("hello")
+const ele = document.getElementById("box1")
+const ele2 = document.getElementById("box2")
+const ele3 = document.getElementById("box3")
+const ele4 = document.getElementById("box4")
+const bt = document.getElementById("btn")
+const box1 = ()=>{
+    ele.style.backgroundColor="red"
+    ele.style.color="orange" 
+    bt.style.backgroundColor="orange"
+}
 
-const demo =()=>{
-    element.textContent="Mahendar"
-    element.style.backgroundColor="red"
+const box2 = ()=>{
+    ele2.style.backgroundColor="green"
+    ele2.style.color="yellow"
+}
+const box3 = ()=>{
+    ele3.style.backgroundColor="orange"
+    ele3.style.color="green"
+}
+const box4 = ()=>{
+    ele4.style.backgroundColor="lightblue"
+    ele4.style.color="white"
 }
